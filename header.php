c366106940213b964b42c8c4218bad104820dc69
ASHOK KUMAR KS <ashokhein@gmail.com>
2014-12-23 00:07:59 +0530

    Initial Commit

---
+++ header.php
@@ -0,0 +1,84 @@
+<!-- Header -->
+<div id="header-wrapper">
+    <div id="header" class="container">
+        
+        <!-- Logo -->
+            <h1 id="logo"><a href="index.php">Recreation Cloud</a></h1>
+            <p>Redefined Hosting Services and Software</p>
+        
+        <!-- Nav -->
+            <nav id="nav">
+                <ul>
+                    <li>
+                        <a href="" class="icon fa-h-square"><span>Domain & Hosting</span></a>
+                        <ul>
+                            <li>
+                                <a href="">Domain Registration</a>
+                                <ul>
+                                    <li><a href="registerdomain.php">Register Domain</a></li>
+                                    <li><a href="#">Bulk Registration</a></li>
+                                    <li><a href="#">Domain Transfer</a></li>
+                                    <li><a href="#">Domain Renewal</a></li>
+                                    <li><a href="#">Private Registration</a></li>
+                                    <li><a href="#">Certified Domain</a></li>
+                                    <li><a href="#">Domain Transfer</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Hosting</a>
+                                <ul>
+                                    <li><a href="#">Hosting Plan</a></li>
+                                    <li><a href="#">Virtual Private Server</a></li>
+                                    <li><a href="#">Dedicated Server</a></li>
+                                    <li><a href="#">Dedicated IP</a></li>
+                                    <li><a href="#">Private Registration</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Add Ons & Security</a>
+                                <ul>
+                                    <li><a href="#">Git Repository</a></li>
+                                    <li><a href="#">SSL Certificate</a></li>
+                                    <li><a href="#">Bulk SMS</a></li>
+                                    <li><a href="#">SEO</a></li>
+                                    <li><a href="#">Social Media</a></li>
+                                    <li><a href="#">Email</a></li>
+                                    <li><a href="#">Analyst and Trend</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                    <a href="" class="icon fa-circle"><span>Opencart Development</span></a>
+                    <ul>
+                        <li> <a href="">Opencart Installation</a> </li>
+                        <li> <a href="">Modules Development</a> </li>
+                        <li> <a href="">Custom Work</a> </li>
+                        <li> <a href="">Module and Theme Installation</a> </li>
+                        <li> <a href="">Payment Integration</a> </li>
+                      </ul>
+                    </li>
+                    <li>
+                    <a href="" class="icon fa-krw"><span>Web Development</span></a>
+                    <ul>
+                        <li> <a href="">PSD -> HTML</a> </li>
+                        <li> <a href="">Web Design Services</a> </li>
+                        <li> <a href="">Custom Development</a> </li>
+                        <li> <a href="">Website ReWork</a> </li>
+                      </ul>
+                    </li>
+                    <li>
+                    <a href="" class="icon fa-ils"><span>Software Development</span></a>
+                    <ul>
+                        <li> <a href="">Opencart Installation</a> </li>
+                        <li> <a href="">Modules Development</a> </li>
+                        <li> <a href="">Custom Work</a> </li>
+                        <li> <a href="">Module and Theme Installation</a> </li>
+                        <li> <a href="">Payment Integration</a> </li>
+                      </ul>
+                    </li>
+                </ul>
+            </nav>
+
+    </div>
+</div>
---
+++ registerdomain.php
@@ -0,0 +1,125 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Domian Name Registration</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<?php include("scriptcss.php"); ?>
+	</head>
+	<body class="homepage">
+
+		<?php include("header.php"); ?>
+		
+
+		<!-- Main -->
+			<div id="main-wrapper">
+				<div id="main" class="container">
+					<div class="row">
+					
+						<!-- Sidebar -->
+							<div id="sidebar" class="4u">
+							
+								<!-- Excerpts -->
+									<section>
+										<ul class="divided">
+											<li>
+
+												<!-- Excerpt -->
+													<article class="box excerpt">
+														<header>
+															<h3>Domain Registration</h3>
+														</header>
+														<p>A domain name is a unique name that identifies an internet resource such as a website. It is an identification string that defines a realm of administrative autonomy, authority or control on the Internet. Domain names are formed by the rules of the Domain Name System (DNS). Any name registered in the DNS is a domain name. The functional description of domain names is presented in the Domain Name System article. Broader usage and industry aspects are captured here.<a href="http://en.wikipedia.org/wiki/Domain_name" target="_new">(wiki)</a></p>
+													</article>
+
+											</li>
+                                            <li>
+
+												<!-- Excerpt -->
+													<article class="box excerpt">
+														<header>
+															<img src="images/free_domain.png" />
+															<h3>Free New Domain</h3>
+														</header>
+														<p>Purchase any 24-month hosting plan and your Domain is free for the first year. Choose from any available .com, .net, .org, .in, .co.in or .edu domain name. </p>
+													</article>
+
+											</li>
+
+											<li>
+
+												<!-- Excerpt -->
+													<article class="box excerpt">
+														<header>
+															<img src="images/domain_privacy.png" />
+															<h3>Free Domain Privacy</h3>
+														</header>
+														<p>Purchase any 24-month hosting plan and your Domain is free for the first year. Choose from any available .com, .net, .org, .in, .co.in or .edu domain name.Domain privacy is a service offered by a number of domain name registrars. A user buys privacy from the company, who in turn replaces the user's info in the WHOIS with the info of a forwarding service (for email and sometimes postal mail, done by a proxy server), such as "Domains by Proxy, Inc." or eNom's "ID Protect".<a href="http://en.wikipedia.org/wiki/Domain_privacy" target="_new">(wiki)</a> </p>
+													</article>
+
+											</li>
+                                          </ul>
+									</section>
+							</div>
+
+						<!-- Content -->
+							<div id="content" class="8u important(collapse)">
+
+								<!-- Post -->
+									<article class="box post">
+										<header>
+											<h2>Domain Registration</h2>
+										</header>
+                                            <form qaid="1" class="form-wrapper-search-box sb" name="RegisterDomainForm" id="RegisterDomainForm" method="POST" onSubmit="return checkDomain(this);" action="http://www.secureserver.net/domains/search.aspx?ci=24122&checkAvail=1&prog_id=506983&pl_id=506983" target="_new">
+                                            <input type="hidden" name="postBack" value="true">
+                                            <input type="hidden" name="Validate" value="0">
+                                            <input type="hidden" name="currStep" value="0">
+                                            <input type="hidden" name="searchSB" value="0">
+                                            <input type="hidden" name="searchSBname" value="">
+                                            <input type="hidden" name="tldReg" value="">
+                                            <input type="hidden" name="searchSBselected" value="">
+                                            <input type="hidden" name="queryOingo" value="">
+                                            <input type="hidden" name="showAltTLDs" value="">
+                                            <input type="hidden" name="fblur" value="0">
+                                            <input type="hidden" name="checkAvail" value="1">
+                                            <input type="text" placeholder="Search here..." required name="domainToCheck" />
+                                            <button type="submit">Search</button>
+                                        </form>
+										<br />
+										<span class="box excerpt">
+											<span class="date">free extras</span>
+										</span>                                        
+										<p><img src="images/forward.png" style="top:2px;position: relative;"/><strong>Domain Forwarding and Masking</strong></p>
+										<p>Direct any domain name you own to your website -- anyone who types that domain name into their browser is taken directly to your website.</p>
+										<p><img src="images/domain-lock.png" style="top:2px;position: relative;"/><strong>Domain Locking</strong></p>
+										<p>Domain locking prevents accidental or intentional transfers of domain ownership and stops anyone from redirecting your nameservers.</p>
+										<p><img src="images/dns_control.png" style="top:2px;position: relative;"/><strong>Total DNS Control</strong></p>
+										<p>Manage your domain nameserver (DNS) records and set your email, FTP, sub-domains and website location -- all from one control panel.</p>										
+										<p><img src="images/change_registration.png" style="top:2px;position: relative;"/><strong>Change of Registration</strong></p>
+										<p>Assign your domain name to someone else or change the contacts for your domain online anytime. Requires a fee for domains.</p>										
+										<p><img src="images/status_alert.png" style="top:2px;position: relative;"/><strong>Status Alerts</strong></p>
+										<p>Monitor the status of your domain and get instant alerts if there's been a change.</p>										
+										<p><img src="images/renewal.png" style="top:2px;position: relative;"/><strong>Auto Renew Protection</strong></p>
+										<p>No need to watch expiration dates to make sure you renew on time! Auto renew keeps your domains, hosting, website builders, and other products in your name and under your control.</p>										
+ 										<span class="box excerpt">
+											<span class="date">Domain Service</span>
+										</span>                                        
+										<p><img src="images/private.png" style="top:2px;position: relative;"/><strong>Private Registration</strong></p>
+										<p>Keeps your personal information private, protecting you from spam, scams and worse.</p>
+										<p><img src="images/idn.png" style="top:2px;position: relative;"/><strong>Internationalized Domain Names (IDN)</strong></p>
+										<p>Register .COM, .NET, .ORG and other popular domain names in any one of over 100 native languages, ranging from Afrikaans to Vietnamese. Search using English or native character sets.</p>
+										<p><img src="images/renewal.png" style="top:2px;position: relative;"/><strong>Domain Transfers</strong></p>
+										<p>Transfer your domains to ReCreationCloud – it's fast, automated and risk-free! You keep all the time remaining on your registration and get a 1-year extension at no extra charge.</p>										
+										<p><img src="images/renewal.png" style="top:2px;position: relative;"/><strong>Domain Backorders</strong></p>
+										<p>Watch the status of any domain currently registered to someone else. Secure your chance to register that domain when it becomes available by <a href="http://www.secureserver.net/domainaddon/domain-alert.aspx?ci=36980&prog_id=506983&pl_id=506983" target="_new">backordering</a> it at special savings.</p>										
+									</article>
+							
+							</div>
+
+					</div>
+				</div>
+			</div>
+		<?php include("footer.php");?>
+	</body>
+</html>
